7bc7e136d625455f11add6bded88335f854afaac
Your Name <you@example.com>
2023-09-27 17:27:11 +0300

    deploy

--- Casks/cocmd.rb
+++ Casks/cocmd.rb
@@ -2,10 +2,10 @@ cask "cocmd" do
   version "1.0.6"
 
   if Hardware::CPU.intel?
-    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-Intel.zip"
+    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/Cocmd-Mac-Intel.zip"
     sha256 "6c880d6bc2eb11271c00dd35d1bf117444cf2a4b1400f93a7e27cc77a5a2bcca"
   else
-    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-M1.zip"
+    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/Cocmd-Mac-M1.zip"
     sha256 "04ed7effebb333cd8ddd951908b9e76f18ef42501b4e9ea350714f4e81a11ff6"
   end
 
