5218ba5d2744a6dc548cb2ff344536b169034496
runner <runner@Mac-1695838386774.local>
2023-09-27 18:39:20 +0000

    Version bump: 1.0.7

--- Casks/cocmd.rb
+++ Casks/cocmd.rb
@@ -1,12 +1,12 @@
 cask "cocmd" do
-  version "1.0.6"
+  version "1.0.7"
 
   if Hardware::CPU.intel?
-    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/Cocmd-Mac-Intel.zip"
-    sha256 "6c880d6bc2eb11271c00dd35d1bf117444cf2a4b1400f93a7e27cc77a5a2bcca"
+    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-Intel.zip"
+    sha256 "8b4a9454521efa9c6399f17f1330ce3926ee3dd02d507ad7e0e5a5ccb6d271f5"
   else
-    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/Cocmd-Mac-M1.zip"
-    sha256 "04ed7effebb333cd8ddd951908b9e76f18ef42501b4e9ea350714f4e81a11ff6"
+    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-M1.zip"
+    sha256 "46c489b6c0f7e6da973a632f2367c37e1f230bbe10e1ad98176a29db20ca3181"
   end
 
   name "Cocmd"
