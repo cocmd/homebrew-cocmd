a754d857196eecbcd3902379aec97065bad0a410
runner <runner@Mac-1695813389396.local>
2023-09-27 12:01:32 +0000

    Version bump: 1.0.6

--- Casks/cocmd.rb
+++ Casks/cocmd.rb
@@ -1,16 +1,16 @@
 cask "cocmd" do
-  version "2.1.8"
+  version "1.0.6"
 
   if Hardware::CPU.intel?
-    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/Cocmd-Mac-Intel.zip"
-    sha256 "a17c03ff341beaf5a286aaab033dc443d703ed41f9dbfae653289dab00120bec"
+    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-Intel.zip"
+    sha256 "6c880d6bc2eb11271c00dd35d1bf117444cf2a4b1400f93a7e27cc77a5a2bcca"
   else
-    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/Cocmd-Mac-M1.zip"
-    sha256 "e84b5a96def1d793997fc8ee4e380f11b1d0515cf120402cd7aec6e07bafb74e"
+    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-M1.zip"
+    sha256 "04ed7effebb333cd8ddd951908b9e76f18ef42501b4e9ea350714f4e81a11ff6"
   end
 
   name "Cocmd"
-  desc "crossplatform sharing of cmd/termina/shell knowledge"
+  desc "A Privacy-first, Cross-platform Text Expander"
   homepage "https://cocmd.org/"
 
   app "Cocmd.app"
