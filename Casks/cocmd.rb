3d06d9c44ed084a484ff325d34d69d681e9bf1fd
runner <runner@Mac-1696772628356.local>
2023-10-08 15:17:58 +0000

    Version bump: 1.0.60

--- Casks/cocmd.rb
+++ Casks/cocmd.rb
@@ -1,19 +1,19 @@
 cask "cocmd" do
-  version "1.0.56"
+  version "1.0.60"
 
   if Hardware::CPU.intel?
     url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-x86_64-apple-darwin.tar.gz"
-    sha256 "fcb01ada01fbc458592af71c0653a6d2e5288c36caf2de275e32e5de1983bc94"
+    sha256 "8c8f52631962a4c5cfe00b42b4a65236544619d4dbe8f7ab0e05d2818f4b92b0"
   else
     url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-aarch64-apple-darwin.tar.gz"
-    sha256 "8d58c40842695cfee95f36b3e500aa48d9763fd00fcee2b82a04889f017f4a7b"
+    sha256 "e949177c00655ec3225980c535b70386d28cd5c5c6cf808c0548fad4a7fd4bc7"
   end
 
   name "Cocmd"
   desc "Streamlined Command Line Operations for Dev Teams"
   homepage "https://cocmd.org/"
 
-  app "Cocmd.app"
+  app "cocmd"
 
   zap trash: "~/Library/Caches/cocmd"
 end
