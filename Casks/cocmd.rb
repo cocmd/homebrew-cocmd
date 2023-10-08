b19c4bf494ed166878b924c2a1675c2a331f48a6
Your Name <you@example.com>
2023-10-08 16:31:19 +0300

    Version bump: 1.0.56

--- Casks/cocmd.rb
+++ Casks/cocmd.rb
@@ -1,16 +1,16 @@
 cask "cocmd" do
-  version "1.0.9"
+  version "1.0.56"
 
   if Hardware::CPU.intel?
-    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-Intel.zip"
-    sha256 "87988b7146d06d00295c92f4c92d0107d5a6aa72576d327c4990b2ccee6c9fd2"
+    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-x86_64-apple-darwin.tar.gz"
+    sha256 "fcb01ada01fbc458592af71c0653a6d2e5288c36caf2de275e32e5de1983bc94"
   else
-    url "https://github.com/Moshe-Roth/cocmd/releases/download/v#{version}/Cocmd-Mac-M1.zip"
-    sha256 "4afb621439e3d0dbb7f429a0589f7c611c243abc3b1dbe27cc6640af384d2115"
+    url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-aarch64-apple-darwin.tar.gz"
+    sha256 "8d58c40842695cfee95f36b3e500aa48d9763fd00fcee2b82a04889f017f4a7b"
   end
 
   name "Cocmd"
-  desc "A Privacy-first, Cross-platform Text Expander"
+  desc "Streamlined Command Line Operations for Dev Teams"
   homepage "https://cocmd.org/"
 
   app "Cocmd.app"
