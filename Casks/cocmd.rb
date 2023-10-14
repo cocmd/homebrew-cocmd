bc3a5c26dbe00502d7a4cde2ee80c5439817e7c7
runner <runner@Mac-1697257477446.local>
2023-10-14 10:01:11 +0000

    Version bump: 1.0.61

--- Casks/cocmd.rb
+++ Casks/cocmd.rb
@@ -1,12 +1,12 @@
 cask "cocmd" do
-  version "1.0.60"
+  version "1.0.61"
 
   if Hardware::CPU.intel?
     url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-x86_64-apple-darwin.tar.gz"
-    sha256 "8c8f52631962a4c5cfe00b42b4a65236544619d4dbe8f7ab0e05d2818f4b92b0"
+    sha256 "92786f90b0ea2de245b8c8eb7ad97f98aac72b8f4d5eb224bf805246e7ad5f98"
   else
     url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-aarch64-apple-darwin.tar.gz"
-    sha256 "e949177c00655ec3225980c535b70386d28cd5c5c6cf808c0548fad4a7fd4bc7"
+    sha256 "7bbbae6428839273eaa17db4cab1f74cc9b62fa0b220c541c8f33524b0fcd458"
   end
 
   name "Cocmd"
