e493d471002ed31367ec23c45b0560e9638c2208
runner <runner@Mac-1699514073399.local>
2023-11-09 08:23:58 +0000

    Version bump: 1.0.78

--- Formula/cocmd.rb
+++ Formula/cocmd.rb
@@ -1,12 +1,12 @@
 class Cocmd < Formula
-  version "1.0.77"
+  version "1.0.78"
 
   if Hardware::CPU.intel?
     url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-x86_64-apple-darwin.tar.gz"
-    sha256 "66fefe93eefe75994b90c8c551036f64afddb0b4dcf6bf1f115a31f9c889ab2f"
+    sha256 "e9e4fb95705bb7324f67a3c8be27ef897754172cc6e89fdddc318a9bfef0da69"
   else
     url "https://github.com/cocmd/cocmd/releases/download/v#{version}/cocmd-aarch64-apple-darwin.tar.gz"
-    sha256 "6278aefd7b1ad061e96ec4893daf0bbcb05a76b21e1c03ccb2f3b18fb81ca285"
+    sha256 "02ffa9c63de47be9f0c32d2ee48eb609b497a4851ba87e9e8af5e0f8d6eab93a"
   end
 
   desc "Streamlined Command Line Operations for Dev Teams"
